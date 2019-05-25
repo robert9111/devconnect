6d1139c836c558dd1daebd630593b7d964ddf316
Robert Perez <Robgaray9111@yahoo.com>
2019-05-25 09:13:12 -0700

    Working on profile schema and routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const user = require('../../models/User');
 
-
-// @route       Get api/profile
+// @route       Get api/profile/me
 // @description Test Route
-// @access      Public
-router.get('/', (req, res) => res.send('Profile Route'));
+// @access      Private
+router.get('/', auth, async (req, res) => {
+    try {
+        const Profile = await Profile.findOne({
+            user: req.user.id
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
